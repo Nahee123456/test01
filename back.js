5561289ffd74b6b1e63bb0bbba9daef428ee0b3b
Nahee123456 <fiw.suphakit@gmail.com>
2025-04-07 14:25:21 +0700

    Add files via upload

---
+++ back.js
@@ -0,0 +1,8 @@
+
+function showmeme(){
+    const meme = document.getElementById("meme");
+    const Box1 = document.querySelector(".Box1")
+    meme.src = "https://i.pinimg.com/236x/0d/43/11/0d43118c794dfa99bfd87af6ba54d19c.jpg";
+    
+    Box1.classList.add("show");
+}
